eff4507210a4a8815777a7c8577cf63d1c5671b8
Leonardo <leonardolima2008@gmail.com>
2020-04-17 15:34:01 -0300

    base da atividade ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {EnvioMensagem} from './Components/EnvioMensagem/EnvioMensagem'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EnvioMensagem />
     </div>
   );
 }
